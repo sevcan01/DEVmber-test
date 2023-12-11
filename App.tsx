470b9715b2c8534da96088d089a9e2e166c531a3
sevcan atacan <sevcanatacan@sevcans-Laptop.lan>
2023-12-11 20:56:56 +0100

    Change app.tsx

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
 
+
+
+const days =[...Array(24)].map((val,index)=>index+1)
 export default function App() {
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+ 
+    <FlatList
+    data={days}
+    contentContainerStyle={styles.content}
+    columnWrapperStyle={styles.columnWrapper}
+    numColumns={2}
+    renderItem={({item})=><DayListItem  day={item} />
+      }
+      showsVerticalScrollIndicator={false}
+
+    />
+
+
+  
+  
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +35,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
+  content: {
+    gap:10,
+    padding:10,
+  
+  },
+  columnWrapper: {
+    gap:10,
+  },
+
 });
