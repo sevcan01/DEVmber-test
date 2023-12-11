dd544ac554fea05f9019bd74c549b14cc3435608
sevcan atacan <sevcanatacan@sevcans-Laptop.lan>
2023-12-11 22:29:41 +0100

    Custom Fonts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 import DayListItem from './src/components/core/DayListItem';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import {AmaticSC_400Regular,AmaticSC_700Bold} from '@expo-google-fonts/amatic-sc';
+import { useEffect } from 'react';
 
-
-
+SplashScreen.preventAutoHideAsync()
 const days =[...Array(24)].map((val,index)=>index+1)
 export default function App() {
-
-
+  const [fontsLoaded,fontError] = useFonts({
+  Inter: Inter_900Black,
+  Amatic: AmaticSC_400Regular,
+  AmaticBold: AmaticSC_700Bold,
+  })
+  useEffect(()=>{
+  
+    if(fontsLoaded || fontError) {
+  
+  SplashScreen.hideAsync()
+    }
+  
+  },[fontsLoaded,fontError])
+  if(!fontsLoaded && !fontError) {
+  return null;
+      }
 
   return (
     <View style={styles.container}>
--- src/components/core/DayListItem.tsx
+++ src/components/core/DayListItem.tsx
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
         text: {
           color: '#9b4521',
           fontSize: 70,
-          fontFamily:'Inter'
+          fontFamily:'AmaticBold'
         },
 
     })
